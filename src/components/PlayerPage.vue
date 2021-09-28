9f9216a12e8454c397f1facd0b75ed44d7030048
井川 拓信 <zin920314@gmail.com>
2021-09-28 23:17:23 +0900

    feat: 待機中のUIを実装

---
+++ src/components/PlayerPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="player-page">
+    <PlayerWaitingScene class="scene" />
+  </section>
+</template>
+
+<script>
+import PlayerWaitingScene from "./PlayerWaitingScene.vue";
+
+export default {
+  components: {
+    PlayerWaitingScene,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.player-page {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/assets/player_background.png");
+  background-size: cover;
+}
+
+.scene {
+  width: 100%;
+  height: 100%;
+}
+</style>
