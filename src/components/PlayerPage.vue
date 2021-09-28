df8adc083a8d4cd684a3e6662e60c0d8308b3aac
井川 拓信 <zin920314@gmail.com>
2021-09-29 01:02:42 +0900

    feat: クイズ読み上げ中のUIを実装

--- src/components/PlayerPage.vue
+++ src/components/PlayerPage.vue
@@ -1,15 +1,41 @@
 <template>
   <section class="player-page">
-    <PlayerWaitingScene class="scene" />
+    <PlayerWaitingScene v-if="isWaiting" class="scene" />
+    <PlayerQuizScene v-if="isGaming" class="scene" :game="game" />
   </section>
 </template>
 
 <script>
 import PlayerWaitingScene from "./PlayerWaitingScene.vue";
+import PlayerQuizScene from "./PlayerQuizScene.vue";
 
 export default {
   components: {
     PlayerWaitingScene,
+    PlayerQuizScene,
+  },
+
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    /**
+     * 待機中か
+     */
+    isWaiting() {
+      return this.game.state === 1;
+    },
+
+    /**
+     * ゲーム中か
+     */
+    isGaming() {
+      return [2].includes(this.game.state);
+    },
   },
 };
 </script>
---
+++ src/components/PlayerQuizScene.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="player-quiz-scene">
+    <PlayerQuizReading class="reading-layer" :quiz="game.quiz" />
+    <div class="overlay" />
+  </div>
+</template>
+
+<script>
+import PlayerQuizReading from "./PlayerQuizReading.vue";
+
+export default {
+  components: {
+    PlayerQuizReading,
+  },
+
+  props: {
+    game: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$READING_LAYER_Z_INDEX: 10;
+
+.player-quiz-scene {
+  position: relative;
+  z-index: 0;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba($color: #000000, $alpha: 0.6);
+  z-index: $READING_LAYER_Z_INDEX - 1;
+}
+
+.reading-layer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: $READING_LAYER_Z_INDEX;
+}
+</style>
