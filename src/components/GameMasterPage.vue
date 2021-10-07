02d4fb0b902c350991d89547743ac03f663d8c3b
井川 拓信 <zin920314@gmail.com>
2021-10-08 01:03:51 +0900

    feat: GameMasterAppのクイズ情報のUIを実装

--- src/components/GameMasterPage.vue
+++ src/components/GameMasterPage.vue
@@ -1,15 +1,19 @@
 <template>
   <section class="game-master-page">
     <GameMasterGameState :game="game" />
+    <GameMasterQuizInfo v-if="showsQuizInfo" :quiz="game.quiz" />
   </section>
 </template>
 
 <script>
+import GameState from "../application/gameState";
 import GameMasterGameState from "./GameMasterGameState.vue";
+import GameMasterQuizInfo from "./GameMasterQuizInfo.vue";
 
 export default {
   components: {
     GameMasterGameState,
+    GameMasterQuizInfo,
   },
 
   props: {
@@ -18,11 +22,25 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    showsQuizInfo() {
+      return [
+        GameState.QUIZ_READING,
+        GameState.QUIZ_ANSWERING,
+        GameState.QUIZ_RESULT,
+      ].includes(this.game.state);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .game-master-page {
   padding: 16px;
+}
+
+.game-master-quiz-info {
+  margin-top: 16px;
 }
 </style>
---
+++ src/components/GameMasterQuizInfo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="game-master-quiz-info">
+    <div class="row">
+      <span class="label">問題:</span>
+      <span class="text">{{ quiz.content }}</span>
+    </div>
+
+    <div v-for="choice in quiz.choices" :key="choice.number" class="row">
+      <span class="label">{{ `選択肢${choice.number}:` }}</span>
+      <span class="text">{{ choice.text }}</span>
+    </div>
+
+    <div class="row">
+      <span class="label">正解:</span>
+      <span class="text">{{ quiz.correct.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quiz: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.row:not(:last-child) {
+  margin-bottom: 24px;
+}
+
+.label {
+  margin-right: 16px;
+  font-weight: bold;
+}
+
+.text {
+  white-space: pre-wrap;
+}
+</style>
